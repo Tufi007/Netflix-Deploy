e1488dc941d56b261dd7cb3a244554edc869a4bf
Tufi007 <taufeeqr346@gmail.com>
2024-03-07 21:51:07 +0530

    try 41

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 
 const dotenv= require('dotenv');
-dotenv.config({path:'./config.env'});
+dotenv.config({path:'./.env'});
 const mongoose= require('mongoose');
 const express = require("express");
 const app = express();
